08754bce95373287da35ad1c2c4547879d8b273b
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-08-24 10:13:37 +0900

    feat: add solution for baekjoon 15688

---
+++ content/baekjoon/15/68/8/_index.md
@@ -0,0 +1,20 @@
++++
+title = "15688"
+tags = ["sorting"]
+math = true
++++
+
+# 수 정렬하기 5
+
+> Level: Silver 5 \
+> Tags: Sorting
+
+## Problem
+
+Sort an array of integers in non-decreasing order.
+
+## Solution
+
+### C++ `std::sort`
+
+> Implementation: [C++](./src/a.cpp) (4208ms, 5928KB) \
---
+++ content/baekjoon/15/68/8/src/a.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+void solution(std::vector<int> &numbers) {
+  std::sort(numbers.begin(), numbers.end());
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int n = 0;
+  std::cin >> n;
+
+  std::vector<int> numbers(n);
+  for (int &number : numbers) {
+    std::cin >> number;
+  }
+
+  solution(numbers);
+
+  for (int number : numbers) {
+    std::cout << number << '\n';
+  }
+
+  return 0;
+}
