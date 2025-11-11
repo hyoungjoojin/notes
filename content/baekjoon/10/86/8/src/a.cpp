957445ce3a7d12e276367f7276a0a1320eaea375
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-11-11 12:32:03 +0900

    feat: add solution for baekjoon 10868

---
+++ content/baekjoon/10/86/8/src/a.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+void solution(std::vector<int> &array, int num_queries) {
+  int size = 1;
+  while (size < array.size()) {
+    size *= 2;
+  }
+
+  std::vector<int> segtree(size * 2, std::numeric_limits<int>::max());
+  for (int i = 0; i < array.size(); i++) {
+    segtree[size + i] = array[i];
+  }
+
+  for (int i = size * 2 - 1; i > 1; i--) {
+    segtree[i / 2] = std::min(segtree[i / 2], segtree[i]);
+  }
+
+  int a = 0, b = 0;
+  while (num_queries--) {
+    std::cin >> a >> b;
+
+    int result = std::numeric_limits<int>::max();
+    for (int left = a + size - 1, right = b + size - 1; left <= right;) {
+      if (left % 2 == 1) {
+        result = std::min(result, segtree[left]);
+      }
+
+      if (right % 2 == 0) {
+        result = std::min(result, segtree[right]);
+      }
+
+      left = (left + 1) / 2;
+      right = (right - 1) / 2;
+    }
+
+    std::cout << result << '\n';
+  }
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int n = 0, m = 0;
+  std::cin >> n >> m;
+
+  std::vector<int> array(n);
+  for (int &i : array) {
+    std::cin >> i;
+  }
+
+  solution(array, m);
+
+  return 0;
+}
