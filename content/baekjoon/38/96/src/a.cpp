00f0e52c6dde67a6aaee2e8738fce88d11f39dcf
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-12-17 12:04:43 +0900

    feat: add solution for baekjoon 3896, 24481, 21921

---
+++ content/baekjoon/38/96/src/a.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+std::vector<int> primes;
+
+void fill_primes() {
+  std::vector<bool> is_prime(1299710, true);
+
+  for (int i = 2; i * i < is_prime.size(); i++) {
+    if (!is_prime[i]) {
+      continue;
+    }
+
+    for (int j = i + i; j < is_prime.size(); j += i) {
+      is_prime[j] = false;
+    }
+  }
+
+  for (int i = 2; i < is_prime.size(); i++) {
+    if (is_prime[i]) {
+      primes.push_back(i);
+    }
+  }
+}
+
+int solution(int x) {
+  if (std::binary_search(primes.begin(), primes.end(), x)) {
+    return 0;
+  }
+
+  int upper = *std::upper_bound(primes.begin(), primes.end(), x);
+
+  return *std::upper_bound(primes.begin(), primes.end(), x) -
+         *(std::lower_bound(primes.begin(), primes.end(), x) - 1);
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  fill_primes();
+
+  int t = 0;
+  std::cin >> t;
+
+  int input = 0;
+  while (t--) {
+    std::cin >> input;
+    std::cout << solution(input) << '\n';
+  }
+
+  return 0;
+}
